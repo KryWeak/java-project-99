a2418fe7a3776ae0fead6a3c648e894a1d71907e
KryWeak <lohovic6@gmail.com>
2025-12-10 05:42:27 +0300

    fix bugs

--- app/src/main/java/hexlet/code/AppApplication.java
+++ app/src/main/java/hexlet/code/AppApplication.java
@@ -1,13 +1,32 @@
 package hexlet.code;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import net.datafaker.Faker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableMethodSecurity
+@OpenAPIDefinition(info = @Info(title = "OpenAPI", version = "1"), security = @SecurityRequirement(name = "BearerAuth")
+)
+@SecurityScheme(type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT", name = "BearerAuth"
+)
 public class AppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
 
+    @Bean
+    public Faker getFaker() {
+        return new Faker();
+    }
 }
