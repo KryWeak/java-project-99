491c8c720b6efd11ab28ed20d7ff32986081fce8
KryWeak <lohovic6@gmail.com>
2025-12-10 06:34:17 +0300

    fix bugs

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,12 +16,6 @@ application {
 group = "hexlet.code"
 version = "0.0.1-SNAPSHOT"
 
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
-}
-
 repositories {
 	mavenCentral()
 }
