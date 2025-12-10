d163d3e7360f3412457ff3863ab4f1dd13557d79
KryWeak <lohovic6@gmail.com>
2025-12-10 05:33:07 +0300

    the exception and mapper directories and their files have been added

---
+++ app/src/main/java/hexlet/code/mapper/TaskMapper.java
@@ -0,0 +1,193 @@
+package hexlet.code.mapper;
+
+import hexlet.code.dto.TaskCreateDTO;
+import hexlet.code.dto.TaskDTO;
+import hexlet.code.dto.TaskUpdateDTO;
+import hexlet.code.model.Label;
+import hexlet.code.model.Task;
+import hexlet.code.model.TaskStatus;
+import hexlet.code.repository.LabelRepository;
+import hexlet.code.repository.TaskStatusRepository;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+import org.openapitools.jackson.nullable.JsonNullable;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Mapper(
+        uses = { JsonNullableMapper.class, ReferenceMapper.class, TaskStatusRepository.class },
+        imports = { TaskStatusRepository.class },
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class TaskMapper {
+
+    @Autowired
+    protected TaskStatusRepository taskStatusRepository;
+
+    @Autowired
+    private LabelRepository labelRepository;
+
+    @Autowired
+    private JsonNullableMapper jsonNullableMapper;
+
+    @Mapping(source = "title", target = "name")
+    @Mapping(source = "content", target = "description")
+    @Mapping(
+            target = "taskStatus",
+            expression = "java(defineStatusFromCreateDTO(dto))"
+    )
+    @Mapping(source = "assigneeId", target = "assignee")
+    @Mapping(
+            target = "labels",
+            expression = "java(defineListLabelFromCreateDTO(dto))"
+    )
+    public abstract Task map(TaskCreateDTO dto);
+
+    @Mapping(source = "name", target = "title")
+    @Mapping(source = "description", target = "content")
+    @Mapping(source = "taskStatus.slug", target = "status")
+    @Mapping(source = "assignee.id", target = "assigneeId")
+    @Mapping(
+            target = "taskLabelIds",
+            expression = "java(defineListIds(model.getLabels()))"
+    )
+    public abstract TaskDTO map(Task model);
+
+    @Mapping(source = "title", target = "name")
+    @Mapping(source = "content", target = "description")
+    @Mapping(
+            target = "taskStatus",
+            expression = "java(defineStatusFromUpdateDTO(dto, model))"
+    )
+    @Mapping(source = "assigneeId", target = "assignee")
+    @Mapping(
+            target = "labels",
+            expression = "java(defineListLabelFromUpdateDTO(dto, model))"
+    )
+    public abstract void update(TaskUpdateDTO dto, @MappingTarget Task model);
+
+    public List<Label> defineListLabel(List<Long> labelIds) {
+        if (labelIds == null) {
+            return null;
+        }
+        List<Label> result = new ArrayList<>();
+        for (Long idLabel: labelIds) {
+            if (idLabel != null) {
+                Label currentLabel = labelRepository.findById(idLabel).orElse(null);
+                if (currentLabel != null) {
+                    result.add(currentLabel);
+                }
+            }
+        }
+        return result;
+    }
+
+    public List<Label> defineListLabelFromUpdateDTO(TaskUpdateDTO data, Task model) {
+        List<Label> oldListLabel = model.getLabels();
+        boolean replace = jsonNullableMapper.isPresent(data.getTaskLabelIds());
+        if (!replace) {
+            return oldListLabel;
+        }
+        if (data == null) {
+            return null;
+        }
+        JsonNullable<List<Long>> labelIdsJson = data.getTaskLabelIds();
+        if (labelIdsJson == null) {
+            return null;
+        }
+        List<Long> labelIds = labelIdsJson.get();
+        if (labelIds == null) {
+            return null;
+        }
+        return defineListLabel(labelIds);
+    }
+
+    public List<Label> defineListLabelFromCreateDTO(TaskCreateDTO data) {
+        if (data == null) {
+            return null;
+        }
+        List<Long> labelIds = data.getTaskLabelIds();
+        if (labelIds == null) {
+            return null;
+        }
+        return defineListLabel(labelIds);
+    }
+
+    public List<Label> defineListLabelFromListIds(List<Long> listIds) {
+        List<Label> result = new ArrayList<>();
+        for (Long idLabel: listIds) {
+            if (idLabel != null) {
+                Label currentLabel = labelRepository.findById(idLabel).orElse(null);
+                if (currentLabel != null) {
+                    result.add(currentLabel);
+                }
+            }
+        }
+        return result;
+    }
+
+    public List<Long> defineListIds(List<Label> labelList) {
+        if (labelList == null) {
+            return null;
+        }
+        List<Long> result = new ArrayList<>();
+        for (Label currentLabel: labelList) {
+            if (currentLabel != null) {
+                result.add(currentLabel.getId());
+            }
+        }
+        return result;
+    }
+
+    public TaskStatus defineStatusFromUpdateDTO(TaskUpdateDTO data, Task model) {
+        TaskStatus oldStatus = model.getTaskStatus();
+        boolean replace = jsonNullableMapper.isPresent(data.getStatus());
+        if (!replace) {
+            return oldStatus;
+        }
+        if (data == null) {
+            return null;
+        }
+        JsonNullable<String> statusJson = data.getStatus();
+        if (statusJson == null) {
+            return null;
+        }
+        String status = statusJson.get();
+        if (status == null) {
+            return null;
+        }
+        return defineStatus(status);
+    }
+
+    public TaskStatus defineStatusFromCreateDTO(TaskCreateDTO data) {
+        if (data == null) {
+            return null;
+        }
+        String status = data.getStatus();
+        if (status == null) {
+            return null;
+        }
+        return defineStatus(status);
+    }
+
+    public TaskStatus defineStatus(String status) {
+        if (status == null) {
+            return null;
+        }
+        if (status.isEmpty()) {
+            return null;
+        }
+        if (taskStatusRepository.findBySlug(status).isPresent()) {
+            return taskStatusRepository.findBySlug(status).get();
+        }
+        return null;
+    }
+}
