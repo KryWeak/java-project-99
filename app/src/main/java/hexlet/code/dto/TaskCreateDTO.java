602a985617c9757e80669269edc9eca41869de0e
KryWeak <lohovic6@gmail.com>
2025-12-10 05:27:37 +0300

    the dto directory and the files in it have been added and edited

---
+++ app/src/main/java/hexlet/code/dto/TaskCreateDTO.java
@@ -0,0 +1,29 @@
+package hexlet.code.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class TaskCreateDTO {
+
+    private Long index;
+
+    @Size(min = 1)
+    private String title;
+
+    private String content;
+
+    @NotNull
+    private String status;
+
+    @JsonProperty("assignee_id")
+    private Long assigneeId;
+
+    private List<Long> taskLabelIds;
+}
